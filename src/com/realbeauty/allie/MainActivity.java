7a6311237976810cdd1431589c780c7f20371e18
Blundell <blundell.app.dev@gmail.com>
2013-03-19 13:25:46 +0000

    initial commit

---
+++ src/com/realbeauty/allie/MainActivity.java
@@ -0,0 +1,13 @@
+package com.realbeauty.allie;
+
+import android.os.Bundle;
+
+import com.actionbarsherlock.app.SherlockActivity;
+
+public class MainActivity extends SherlockActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
