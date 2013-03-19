58414d30ee16189dd4cca01c51548624359a10be
Blundell <blundell.app.dev@gmail.com>
2013-03-19 13:44:59 +0000

    initial setup of actionbar

--- src/com/realbeauty/allie/MainActivity.java
+++ src/com/realbeauty/allie/MainActivity.java
@@ -3,11 +3,20 @@ package com.realbeauty.allie;
 import android.os.Bundle;
 
 import com.actionbarsherlock.app.SherlockActivity;
+import com.actionbarsherlock.view.Menu;
+import com.actionbarsherlock.view.MenuInflater;
 
 public class MainActivity extends SherlockActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getSupportMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return true;
     }
 }
